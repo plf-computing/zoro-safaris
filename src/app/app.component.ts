8b1820f8156db944365b1fad9768e8d528cd3eaf
plf-computing <plfcomputing@gmail.com>
2024-09-18 18:47:53 +0300

    "Added WhyComponent to app.component.html and app.component.ts"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,11 @@ import { initFlowbite } from 'flowbite';
 import { HeroComponent } from './homePage/hero/hero.component';
 import { DiscoverComponent } from "./homePage/discover/discover.component";
 import { PictureComponent } from './homePage/picture/picture.component';
+import { WhyComponent } from './homePage/why/why.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HeroComponent, DiscoverComponent,PictureComponent],
+  imports: [RouterOutlet, NavbarComponent, HeroComponent, DiscoverComponent,PictureComponent,WhyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
