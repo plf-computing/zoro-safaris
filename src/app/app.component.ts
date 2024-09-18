1390a3bd911714f06c040e774decf300e3f8273d
plf-computing <plfcomputing@gmail.com>
2024-09-18 14:15:44 +0300

    "Added PictureComponent to app.component, modified HTML structure in discover.component and hero.component"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { NavbarComponent } from "./homePage/navbar/navbar.component";
 import { initFlowbite } from 'flowbite';
 import { HeroComponent } from './homePage/hero/hero.component';
 import { DiscoverComponent } from "./homePage/discover/discover.component";
+import { PictureComponent } from './homePage/picture/picture.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HeroComponent, DiscoverComponent],
+  imports: [RouterOutlet, NavbarComponent, HeroComponent, DiscoverComponent,PictureComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
