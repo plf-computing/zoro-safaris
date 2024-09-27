1798dcf228851138a1af3271f456a6cc08cef350
plf-computing <plfcomputing@gmail.com>
2024-09-27 17:18:23 +0300

    "Updated app routes, footer, hero, and navbar components with new logo, styles, and navigation links"

--- src/app/homePage/navbar/navbar.component.ts
+++ src/app/homePage/navbar/navbar.component.ts
@@ -1,12 +1,23 @@
+
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  onLinkClick() {
+    this.isMenuOpen = false; // Close the menu when a link is clicked
+  }
 
 }
