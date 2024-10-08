dda6e2dd6d8e6d84d5ae1677000421520eaeb73b
plf-computing <plfcomputing@gmail.com>
2024-10-08 16:59:04 +0300

    "Updated HTML and TypeScript files in various components to modify layout, styling, and routing."

--- src/app/home/safari-blu/safari-blu.component.ts
+++ src/app/home/safari-blu/safari-blu.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-safari-blu',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './safari-blu.component.html',
   styleUrl: './safari-blu.component.css'
 })
 export class SafariBluComponent {
+
 
 }
--- src/app/home/safaris/safaris.component.ts
+++ src/app/home/safaris/safaris.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-safaris',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './safaris.component.html',
   styleUrl: './safaris.component.css'
 })
 export class SafarisComponent {
+  constructor(private router: Router) { }
+
+  
+  goToSafari(id: number) {
+    this.router.navigate([`/safaris/${id}`]);
+  }
 
 }
