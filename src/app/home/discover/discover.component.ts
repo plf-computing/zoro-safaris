ec67eee7bd453ce86a34262b00ce637b37bf532c
plf-computing <plfcomputing@gmail.com>
2024-10-08 15:01:45 +0300

    "Updated Angular application code: modified component templates, styles, and TypeScript files; added Material Design components and typography; removed Flowbite CSS and JavaScript files."

--- src/app/home/discover/discover.component.ts
+++ src/app/home/discover/discover.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-discover',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './discover.component.html',
   styleUrl: './discover.component.css'
 })
