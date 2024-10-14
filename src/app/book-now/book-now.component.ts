73c7a0ee707c35c802ca4e6d446e5e80ba554de1
plf-computing <plfcomputing@gmail.com>
2024-10-14 18:20:29 +0300

    "Updated HTML and TypeScript files in various components, including About Us, Book Now, Footer, Safari Blu, Safaris, Excursion Quotation, and Quotation, with changes to layout, styling, and form validation."

--- src/app/book-now/book-now.component.ts
+++ src/app/book-now/book-now.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ExcursionQuotationComponent } from "../quotation/excursion-quotation/excursion-quotation.component";
+import { QuotationComponent } from "../quotation/quotation.component";
 
 @Component({
   selector: 'app-book-now',
   standalone: true,
-  imports: [],
+  imports: [ExcursionQuotationComponent, QuotationComponent],
   templateUrl: './book-now.component.html',
   styleUrl: './book-now.component.css'
 })
